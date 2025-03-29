87607e7bf95f5adb04fd2f81f800be4128697b85
tanluc1e <t.luc497303@gmail.com>
2025-03-29 22:50:15 +0700

    chore: add shadcn/ui and change font to Inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata, Viewport } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Inter } from 'next/font/google';
 
 import './globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin']
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin']
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter'
 });
 
 const APP_NAME = 'Minimil';
@@ -101,10 +96,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {children}
+    <html lang='en' suppressHydrationWarning>
+      <body className={`${inter.variable} font-sans antialiased`}>
+        <main className='mx-auto max-w-screen-sm px-6 py-24'>{children}</main>
       </body>
     </html>
   );
