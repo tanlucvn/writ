daa37a4a108a83eab2a0059d9afcb6ab0358c87b
tanluc1e <t.luc497303@gmail.com>
2025-03-31 13:06:55 +0700

    feat: add shadcn, logo and remake homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
-import { Button } from '@/components/ui/button';
+import Logo from '@/components/logo';
+import * as FadeIn from '@/components/motion/fade';
 
 export default function Home() {
   return (
-    <>
-      <Button>Hello world</Button>
-    </>
+    <FadeIn.Container>
+      <FadeIn.Item>
+        <div className='flex flex-row items-center space-x-3'>
+          <Logo />
+          <div className='flex flex-col font-medium'>
+            <span className='text-sm'>Minimil</span>
+            <span className='text-xs text-muted-foreground'>
+              A sleek and minimal Next.js starter template
+            </span>
+          </div>
+        </div>
+      </FadeIn.Item>
+    </FadeIn.Container>
   );
 }
