d2fa781dbbbd7011749ceee322b896855e329704
tanluc1e <t.luc497303@gmail.com>
2025-04-25 14:36:56 +0700

    feat: add deletable for tag chip

--- src/components/settings/tags-section.tsx
+++ src/components/settings/tags-section.tsx
@@ -69,6 +69,7 @@ export default function TagsSection() {
             <TagChip
               key={tag.id}
               tag={tag}
+              deletable
               onClick={() => handleDelete(tag.id)}
             />
           ))
--- src/components/tag-chip.tsx
+++ src/components/tag-chip.tsx
@@ -7,15 +7,22 @@ import { CircleIcon, XIcon } from "lucide-react";
 type TagChipProps = {
   tag: Tag;
   className?: string;
+  deletable?: boolean;
   onClick?: (tag: Tag) => void;
 };
 
-export default function TagChip({ tag, className, onClick }: TagChipProps) {
+export default function TagChip({
+  tag,
+  className,
+  deletable = false,
+  onClick,
+}: TagChipProps) {
   return (
     <div
       onClick={() => onClick?.(tag)}
       className={cn(
-        "group relative flex cursor-pointer select-none items-center gap-1 rounded-full border border-border px-2 py-0.5 text-foreground text-xs outline-double outline-2 outline-border outline-offset-2 hover:border-destructive",
+        "relative flex cursor-pointer select-none items-center gap-1 rounded-full border border-border px-2 py-0.5 text-foreground text-xs outline-double outline-2 outline-border outline-offset-2",
+        deletable && "group hover:border-destructive",
         className,
       )}
     >
@@ -23,7 +30,10 @@ export default function TagChip({ tag, className, onClick }: TagChipProps) {
         fill={tag.color ? tag.color : "#ffffff"}
         className="size-3 group-hover:hidden"
       />
-      <XIcon className="hidden size-3 text-destructive group-hover:block" />
+
+      {deletable && (
+        <XIcon className="hidden size-3 text-destructive group-hover:block" />
+      )}
 
       {tag.name}
     </div>
